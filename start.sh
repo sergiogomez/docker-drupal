48e9d03d0334e5e2692e5d19159655b999506bce
Sergio Gómez <sergio@quaip.com>
2014-06-10 13:57:20 +0200

    Several fixes

--- start.sh
+++ start.sh
@@ -14,7 +14,9 @@ if [ ! -f /var/www/sites/default/settings.php ]; then
 	echo $DRUPAL_PASSWORD > /drupal-db-pw.txt
 	mysqladmin -u root password $MYSQL_PASSWORD 
 	mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE drupal; GRANT ALL PRIVILEGES ON drupal.* TO 'drupal'@'localhost' IDENTIFIED BY '$DRUPAL_PASSWORD'; FLUSH PRIVILEGES;"
-	sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/sites-available/default
+	sed -i 's/www\/html/www\n\t<Directory \/var\/www>\n\t\tAllowOverride All\n\t<\/Directory>\n/' /etc/apache2/sites-available/000-default.conf
+	echo "\$conf['drupal_http_request_fails'] = FALSE;" >> /var/www/sites/default/default.settings.php
+	echo "apc.rfc1867 = 1" >> /etc/php5/apache2/php.ini
 	a2enmod rewrite vhost_alias
 	cd /var/www/
 	drush site-install standard -y --account-name=admin --account-pass=admin --db-url="mysqli://drupal:${DRUPAL_PASSWORD}@localhost:3306/drupal"
